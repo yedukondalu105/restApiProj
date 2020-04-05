c56f0c53fd46a6f85e2bf1f920239e3868d2ac07
yedukondalu105 <uppalayk105@gmail.com>
2020-04-05 18:28:00 +0530

    Commiting updated file from staging area directly

---
+++ src/test/java/wells/restApiProj/TestStagingDirectCommitCheck.java
@@ -0,0 +1,10 @@
+package wells.restApiProj;
+
+public class TestStagingDirectCommitCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is new change2");
+	}
+
+}
